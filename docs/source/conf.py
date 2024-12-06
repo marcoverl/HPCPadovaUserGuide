1857d8e4e1b08163cc289f9e9f4b2e67cfa06ba6
Massimo Sgaravatto <sgaravat@localhost.localdomain>
2024-12-06 11:39:52 +0100

    Scheleton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HPC cluster at INFN Padova'
+copyright = '2021, HPC INFN Padova Support Team'
+author = 'HPC INFN Padova Support Team'
 
 release = '0.1'
 version = '0.1.0'
---
+++ docs/source/storage.rst
@@ -0,0 +1,3 @@
+Storage in the HPC cluster
+==========================
+
