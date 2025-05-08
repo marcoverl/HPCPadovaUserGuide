902a9d1818565c9cab7ad97709bea1644cc60d19
Massimo Sgaravatto <sgaravat@lxsgaravat.pd.infn.it>
2025-05-08 13:12:16 +0200

    Revisione di Marco

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'HPC cluster at INFN Padova'
 copyright = '2021, HPC INFN Padova Support Team'
 author = 'HPC INFN Padova Support Team'
 
-release = '0.17'
-version = '0.17.0'
+release = '0.18'
+version = '0.18.0'
 
 # -- General configuration
 
